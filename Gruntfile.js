712f7eecc9b0b77a2783caf3986aca2780e63bb4
Kaizhi Wei <hello@kaizhiwei.com>
2013-11-23 14:25:35 -0500

    add beta tag to title, remove compass debug from watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,7 +164,7 @@ module.exports = function (grunt) {
             dist: {},
             server: {
                 options: {
-                    debugInfo: true
+                    debugInfo: false
                 }
             }
         },
